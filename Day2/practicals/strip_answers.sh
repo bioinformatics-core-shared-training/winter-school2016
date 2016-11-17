91a120258a1fc84f60add0a513d8460e70fcfdce
Aaron Lun <alun@wehi.edu.au>
2016-11-17 17:29:54 +0000

    Added practical source files for plotting, simple tests, linear models.

---
+++ Day2/practicals/strip_answers.sh
@@ -0,0 +1,7 @@
+# This takes a source file and generates the corresponding question and answer files.
+cat teaching_R_source.Rmd | grep -v "## ANSWER ##" > teaching_R_questions.Rmd
+cat teaching_R_source.Rmd | sed  "s/## ANSWER ##//" | grep -v "# Put.*!" > teaching_R_answers.Rmd
+
+cat test_R_source.Rmd | grep -v "## ANSWER ##" > test_R_questions.Rmd
+cat test_R_source.Rmd | sed  "s/## ANSWER ##//" | grep -v "# Put.*!" > test_R_answers.Rmd
+
